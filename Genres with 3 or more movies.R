3b78c5fe0efeb9eecb8a1c585ddc892928601de1
swinters2 <swinter2@nd.edu>
2023-10-12 14:31:39 -0400

    code updates

--- Genres with 3 or more movies.R
+++ Genres with 3 or more movies.R
@@ -1,38 +1,35 @@
 
 
-read.csv("C:/Users/sydwi/Downloads/movie_genres.csv")
+# read.csv("data/movie_genres.csv")
 
 library(dplyr)
-install.packages("tidyverse")
+# install.packages("tidyverse")
 library(tidyverse)
 
 ####What is the top 5 movie genres in the database
 
-my_data <- read.csv("C:/Users/sydwi/Downloads/movie_genres.csv")
+my_data <- read.csv("data/movie_genres.csv")
 
 head(my_data)
 
-movie_count <- 
+
 
 top_movies <- my_data %>%
   group_by(genre) %>%
   summarise(movie_count = n()) %>%
   filter(movie_count >= 3)
 
-view(movie_count)
+# view(movie_count)
 
 top_movies <- top_movies %>%
   arrange(desc(movie_count))
 
-top_movies
+# top_movies
 
+top_movies$genre <- reorder(top_movies$genre, top_movies$movie_count)
 
-install.packages("ggplot2")
+# install.packages("ggplot2")
 library(ggplot2)
-
-
-
-
 
 ggplot(top_movies, aes(x = genre, y = movie_count, fill = genre)) + 
   geom_bar(stat = "identity", color = "black") +
